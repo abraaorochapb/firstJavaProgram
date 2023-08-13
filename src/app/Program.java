0e2c77b9eb28311d42b9cd15d1b4d69ddda97599
abraaorochapb <abraaorochapb@gmail.com>
2023-08-13 16:13:40 -0300

    Exercício de fixação

--- src/app/Program.java
+++ src/app/Program.java
@@ -1,5 +1,7 @@
 package app;
 
+import java.util.Locale;
+
 public class Program {
     public static void main(String[] args) {
         System.out.println("Hello World!");
@@ -23,6 +25,32 @@ public class Program {
         // %2f = duas casas decimais.
 
         System.out.printf("%s tem %d anos e ganha R$ %.2f\n", name, age, salary);
+
+
+        String product1 = "Computer";
+        String product2 = "Office desk";
+
+        // Exercício de fixação
+
+        //Setando as variáveis
+        int age2 = 30;
+        int code = 5290;
+        char gender = 'F';
+
+        double price1 = 2100.0;
+        double price2 = 650.50;
+        double measure = 53.234567;
+
+        //Outputs
+
+        System.out.printf("Products:\n%s, wich price is U$ %.2f\n%s, which price is U$ %.2f\n", product1, price1, product2, price2);
+
+        System.out.printf("Record: %d years old, code %d and gender: %c%n", age2, code, gender);
+
+        System.out.printf("Measure with eight decimal places: %.8f%n", measure);
+        System.out.printf("Rouded (three decimal places): %.3f%n", measure);
+        Locale.setDefault(Locale.US);
+        System.out.printf("US decimal point: %.3f%n", measure);
     }
 
 }
